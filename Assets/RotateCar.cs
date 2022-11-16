61f8c303f8241dc878c3f1c4da0ddecaff692518
anafful <117636756+anafful@users.noreply.github.com>
2022-11-16 09:50:08 +0000

    rotatescript

--- Assets/RotateCar.cs
+++ Assets/RotateCar.cs
@@ -7,19 +7,20 @@ public class RotateCar : MonoBehaviour
 
     public float RotationSpeed = 2f;
     // Start is called before the first frame update
-  
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetMouseButton(0))
+        {
+            float XaxisRotation = Input.GetAxis("Mouse X") * RotationSpeed;
+
+            transform.Rotate(Vector3.up, XaxisRotation);
+
+        }
 
 
-    private void OnMouseDown()
-    {
-        float XaxisRotation = Input.GetAxis("Mouse X") * RotationSpeed;
 
-        transform.Rotate(Vector3.down, XaxisRotation);
     }
 }
