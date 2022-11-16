94fa321190ce82028a5dada9734ab593a12f8b06
anafful <117636756+anafful@users.noreply.github.com>
2022-11-16 09:22:09 +0000

    script

---
+++ Assets/menubuttons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menubuttons : MonoBehaviour
+{
+    public GameObject Menupanel;
+    public GameObject MainMenupanel;
+
+    public void OpenMenu()
+    {
+        if (Menupanel != null)
+        {
+            Menupanel.SetActive(true);
+            MainMenupanel.SetActive(false);
+        }
+
+
+
+    }
+
+    public void CloseMenu()
+    {
+        Menupanel.SetActive(true);
+    }
+}
