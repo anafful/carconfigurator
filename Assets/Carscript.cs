5abf629b8ed4599ab1954cd0e2431dcf741ec48e
anafful <117636756+anafful@users.noreply.github.com>
2022-11-18 11:04:42 +0000

    Cinemachine

--- Assets/Carscript.cs
+++ Assets/Carscript.cs
@@ -29,7 +29,7 @@ public class Carscript : MonoBehaviour
 
 
 
-    carclass carclass = new carclass();
+    //carclass carclass = new carclass();
 
 
 
--- Assets/menubuttons.cs
+++ Assets/menubuttons.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class menubuttons : MonoBehaviour
 {
     public GameObject Menupanel;
     public GameObject MainMenupanel;
+    [SerializeField] GameObject[] CarCameras;
+
+    private int CurrentCamera;
+
+
+
+    private void Update()
+    {
+       
+    }
+
 
     public void OpenMenu()
     {
@@ -24,4 +36,35 @@ public class menubuttons : MonoBehaviour
         Menupanel.SetActive(false);
         MainMenupanel.SetActive(true);
     }
+
+
+    public void CameraSwitch()
+    {
+        CurrentCamera = 0;
+
+        for(int i = 0; i < CarCameras.Length; i++)
+        {
+            CarCameras[i].gameObject.SetActive(false);
+            Debug.Log("switch camera");
+        }
+        //if(CarCameras.Length > 0)
+        {
+          //  CarCameras[0].gameObject.SetActive(true);
+        }
+
+        //Cinecameraloop();
+    }
+
+     public void Cinecameraloop()
+    {
+        if (CarCameras.Length != 0)
+        {
+            foreach (GameObject camera in CarCameras)
+            {
+                camera.GetComponent<CinemachineVirtualCamera>().Priority = Random.Range(10, 8);
+            }
+        }
+    }
+     
+
 }
